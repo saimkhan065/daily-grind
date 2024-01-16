6e54be148108bb62efcf314ecbe0c61facd332f7
Saim Khan <saimkhan065@gmail.com>
2024-01-16 11:35:37 -0500

    learnt AJAX with async/await. Success on fetching API.

--- jsThings/async.js
+++ jsThings/async.js
@@ -1,1 +1,10 @@
-//Test file for first commit :)
+console.log("JS");
+async function getWeatherAW(latitude, longitude) {
+  const result = await fetch(
+    `https://api.weather.gov/points/${latitude},${longitude}`
+  );
+  const data = await result.json();
+  console.log(data);
+}
+
+getWeatherAW(39.7456, -97.0892);
