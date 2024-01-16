291e60535cfab671d1821dd0ca0d4e535d184579
Saim Khan <saimkhan065@gmail.com>
2024-01-16 17:09:40 -0500

    added some project files

--- jsThings/async.js
+++ jsThings/async.js
@@ -1,10 +1,14 @@
 console.log("JS");
 async function getWeatherAW(latitude, longitude) {
-  const result = await fetch(
-    `https://api.weather.gov/points/${latitude},${longitude}`
-  );
-  const data = await result.json();
-  console.log(data);
+  try {
+    const result = await fetch(
+      `https://api.weather.gov/points/${latitude},${longitude}`
+    );
+    const data = await result.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 getWeatherAW(39.7456, -97.0892);
