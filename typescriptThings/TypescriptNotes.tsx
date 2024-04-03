9c60ff9e431f8a79f3a631d243898ee5667b65ab
Saim Khan <saimkhan065@gmail.com>
2024-04-03 11:33:35 -0400

    setup a TS project

---
+++ typescriptThings/TypescriptNotes.tsx
@@ -0,0 +1,6 @@
+//Setting up a TS project
+//1. initialize project with npm init
+//2. set up a ts project with npx tsc --init
+//3. compile project with npx tsc {filename}.tsx
+
+let userName: string = "";
